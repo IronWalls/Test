4f57c4bcee838790f7980475d54ebb9a7cf78eee
DOG007ua <56601759+DOG007ua@users.noreply.github.com>
2021-02-28 11:53:53 +0200

    Creating a scriptable obj to set the scene value

--- Assets/_Complete-Game/Scripts/BoardManager.cs
+++ Assets/_Complete-Game/Scripts/BoardManager.cs
@@ -24,8 +24,19 @@ namespace Completed
             }
         }
 
+        public int columns => levelParams.columns; //Number of columns in our game board.
+        public int rows => levelParams.rows; //Number of rows in our game board.
+        public Count wallCount => levelParams.wallCount; //Lower and upper limit for our random number of walls per level.
+        public Count foodCount => levelParams.foodCount; //Lower and upper limit for our random number of food items per level.
+        public GameObject exit => levelParams.exit; //Prefab to spawn for exit.
+        public GameObject[] floorTiles => levelParams.floorTiles; //Array of floor prefabs.
+        public GameObject[] wallTiles => levelParams.wallTiles; //Array of wall prefabs.
+        public GameObject[] foodTiles => levelParams.foodTiles; //Array of food prefabs.
+        public GameObject[] enemyTiles => levelParams.enemyTiles; //Array of enemy prefabs.
+        public GameObject[] outerWallTiles => levelParams.outerWallTiles; //Array of outer tile prefabs.
 
-        public int columns = 8; //Number of columns in our game board.
+
+        /*public int columns = 8; //Number of columns in our game board.
         public int rows = 8; //Number of rows in our game board.
         public Count wallCount = new Count(5, 9); //Lower and upper limit for our random number of walls per level.
         public Count foodCount = new Count(1, 5); //Lower and upper limit for our random number of food items per level.
@@ -34,8 +45,9 @@ namespace Completed
         public GameObject[] wallTiles; //Array of wall prefabs.
         public GameObject[] foodTiles; //Array of food prefabs.
         public GameObject[] enemyTiles; //Array of enemy prefabs.
-        public GameObject[] outerWallTiles; //Array of outer tile prefabs.
+        public GameObject[] outerWallTiles; //Array of outer tile prefabs.*/
 
+        [SerializeField]private LevelParams levelParams;
         private Transform boardHolder; //A variable to store a reference to the transform of our Board object.
         private List<Vector3> gridPositions = new List<Vector3>(); //A list of possible locations to place tiles.
 
---
+++ Assets/_Complete-Game/Scripts/LevelParams/LevelParams.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Completed.BoardManager;
+
+[CreateAssetMenu(fileName = "LevelParameters", menuName = "ScriptableObjects/SetLevelParameters", order = 1)]
+public class LevelParams : ScriptableObject
+{
+    public int columns = 8; //Number of columns in our game board.
+    public int rows = 8; //Number of rows in our game board.
+    public Count wallCount = new Count(5, 9); //Lower and upper limit for our random number of walls per level.
+    public Count foodCount = new Count(1, 5); //Lower and upper limit for our random number of food items per level.
+    public GameObject exit; //Prefab to spawn for exit.
+    public GameObject[] floorTiles; //Array of floor prefabs.
+    public GameObject[] wallTiles; //Array of wall prefabs.
+    public GameObject[] foodTiles; //Array of food prefabs.
+    public GameObject[] enemyTiles; //Array of enemy prefabs.
+    public GameObject[] outerWallTiles; //Array of outer tile prefabs.
+}
