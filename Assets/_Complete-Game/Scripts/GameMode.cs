7ea9f95d6ea60beb5a817953b1c553d22d51c688
Dmitriy Starohorodskyi <catereiptsi@gmail.com>
2021-03-11 18:27:32 +0200

    [Question 1] Implement new game mode configurations

---
+++ Assets/_Complete-Game/Scripts/GameMode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Mode", fileName = "New Game Mode")]
+public class GameMode : ScriptableObject
+{
+    public float levelStartDelay = 2f ; //Time to wait before starting level, in seconds.
+    public float turnDelay = 0.1f; //Delay between each Player turn.
+    public int playerFoodPoints = 100; //Starting value for Player food points.
+}
