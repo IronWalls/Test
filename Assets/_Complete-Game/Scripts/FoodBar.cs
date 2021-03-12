e2b898ae75bfe068a04c137783bbd709b2306810
Maksym Hernets <gernets.maksim@gmail.com>
2021-03-12 11:19:03 +0200

    Commit 4 fix

--- Assets/_Complete-Game/Scripts/FoodBar.cs
+++ Assets/_Complete-Game/Scripts/FoodBar.cs
@@ -5,20 +5,17 @@ using UnityEngine.UI;
 
 public class FoodBar : MonoBehaviour
 {
-	public GameObject Target;
-    private Image image;
-
-	private void Start()
-	{
-		image = gameObject.GetComponent<Image>();
-	}
+	public GameObject Target; // Target gameobject player
+    public Image image; 
 
 	private void Update()
 	{
-		var playerOnScreen = Camera.main.WorldToScreenPoint(Target.transform.localPosition);
-		transform.localPosition = new Vector3( playerOnScreen.x, playerOnScreen.y, 0) + new Vector3(0, 10, 0);
+		// Purpose following
+		var playerOnScreen = Camera.main.WorldToScreenPoint(Target.gameObject.transform.position);
+		transform.position = new Vector3( playerOnScreen.x, playerOnScreen.y, 0) + new Vector3(0, 50, 0);
 	}
 
+	// Setting up a food bar
 	public void SetBar(int MaxBar, int value)
 	{
 		image.fillAmount = (float)value /(float)MaxBar;
