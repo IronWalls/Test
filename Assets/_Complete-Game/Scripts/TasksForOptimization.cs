bccbf7a995c543d3e5a1e7ee359874865822f83b
Alex Wyshnya <sasha.wyshnya@gmail.com>
2021-03-21 17:53:46 +0200

    added list of suggestions

---
+++ Assets/_Complete-Game/Scripts/TasksForOptimization.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TasksForOptimization : MonoBehaviour
+{
+    // list of tasks (dunno if you wanted actual <List>)
+    // I will probably suggest more if was working longer with it, but heres what i can say from the top of my head
+    // 1). i would suggest to check project and to change all calls using invokes and maybe use corutines instead - 2 hours maybe
+    // 2). Also in several places i saw usage of:  Find.***.etc...  those things also aren't efficient so worth rewriting 3-4 hours maybe
+    // 3). didn't managed to check all systems but the way the level is loaded also i think can be approved - 4+ hours i guess
+    // 
+}
